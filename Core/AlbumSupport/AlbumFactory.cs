e13d0b9056ca8e5d8ed06be019bf0b614a6c78c7
ashmind <ashmind@gmail.com>
2010-10-18 11:45:59 +0400

    Added lost files.

---
+++ Core/AlbumSupport/AlbumFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using AshMind.Gallery.Core.IO;
+using AshMind.Gallery.Core.Commenting;
+
+namespace AshMind.Gallery.Core.AlbumSupport {
+    public class AlbumFactory {
+        private readonly IAlbumNameTransform[] nameTransforms;
+
+        public AlbumFactory(IAlbumNameTransform[] nameTransforms) {
+            this.nameTransforms = nameTransforms;
+        }
+
+        public Album Create(
+            AlbumDescriptor descriptor,
+            string proposedName, Func<IList<AlbumItem>> itemsFactory,
+            object securableToken
+        ) {
+            var name = nameTransforms.Aggregate(proposedName, (n, t) => t.Transform(n, descriptor));
+
+            return new Album(
+                descriptor,
+                name, itemsFactory,
+                securableToken
+            );
+        }
+    }
+}
